d5b2d275e41ebf7c73330b249cc8c980c655f82b
hieu-pv <hieupv.est@gmail.com>
2020-11-05 11:25:19 +0700

    1 file modified

--- src/app/demo/extend/extend.component.ts
+++ src/app/demo/extend/extend.component.ts
@@ -1,44 +1,94 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { NgxFormGroup } from '@ngxform/platform';
-import { Validators } from '@angular/forms';
-import { NgxBootstrapTypeaheadControl, WindowTemplateContext } from '@ngxform/ng-bootstrap-typeahead';
+import { Component, OnInit, TemplateRef, ViewChild } from "@angular/core";
+import { NgxFormGroup } from "@ngxform/platform";
+import { Validators } from "@angular/forms";
+import {
+  NgxBootstrapTypeaheadControl,
+  WindowTemplateContext
+} from "@ngxform/ng-bootstrap-typeahead";
+import { delay } from "rxjs/internal/operators";
+import { of, Subject } from "rxjs";
 
 @Component({
-  selector: 'app-extend',
-  templateUrl: './extend.component.html',
-  styleUrls: ['./extend.component.scss']
+  selector: "app-extend",
+  templateUrl: "./extend.component.html",
+  styleUrls: ["./extend.component.scss"]
 })
 export class ExtendComponent implements OnInit {
   public demoForm: NgxFormGroup;
 
-  @ViewChild('windowTemplate', { static: true }) windowTemplate: TemplateRef<WindowTemplateContext>;
+  @ViewChild("windowTemplate", { static: true }) windowTemplate: TemplateRef<
+    WindowTemplateContext
+  >;
 
   ngOnInit(): void {
     const typeaheadOptions: { name: string; flag: string }[] = [
-      { name: 'Alabama', flag: '5/5c/Flag_of_Alabama.svg/45px-Flag_of_Alabama.svg.png' },
-      { name: 'Alaska', flag: 'e/e6/Flag_of_Alaska.svg/43px-Flag_of_Alaska.svg.png' },
-      { name: 'Arizona', flag: '9/9d/Flag_of_Arizona.svg/45px-Flag_of_Arizona.svg.png' },
-      { name: 'Arkansas', flag: '9/9d/Flag_of_Arkansas.svg/45px-Flag_of_Arkansas.svg.png' },
-      { name: 'California', flag: '0/01/Flag_of_California.svg/45px-Flag_of_California.svg.png' },
-      { name: 'Colorado', flag: '4/46/Flag_of_Colorado.svg/45px-Flag_of_Colorado.svg.png' },
-      { name: 'Connecticut', flag: '9/96/Flag_of_Connecticut.svg/39px-Flag_of_Connecticut.svg.png' },
-      { name: 'Delaware', flag: 'c/c6/Flag_of_Delaware.svg/45px-Flag_of_Delaware.svg.png' },
-      { name: 'Florida', flag: 'f/f7/Flag_of_Florida.svg/45px-Flag_of_Florida.svg.png' }
+      {
+        name: "Alabama",
+        flag: "5/5c/Flag_of_Alabama.svg/45px-Flag_of_Alabama.svg.png"
+      },
+      {
+        name: "Alaska",
+        flag: "e/e6/Flag_of_Alaska.svg/43px-Flag_of_Alaska.svg.png"
+      },
+      {
+        name: "Arizona",
+        flag: "9/9d/Flag_of_Arizona.svg/45px-Flag_of_Arizona.svg.png"
+      },
+      {
+        name: "Arkansas",
+        flag: "9/9d/Flag_of_Arkansas.svg/45px-Flag_of_Arkansas.svg.png"
+      },
+      {
+        name: "California",
+        flag: "0/01/Flag_of_California.svg/45px-Flag_of_California.svg.png"
+      },
+      {
+        name: "Colorado",
+        flag: "4/46/Flag_of_Colorado.svg/45px-Flag_of_Colorado.svg.png"
+      },
+      {
+        name: "Connecticut",
+        flag: "9/96/Flag_of_Connecticut.svg/39px-Flag_of_Connecticut.svg.png"
+      },
+      {
+        name: "Delaware",
+        flag: "c/c6/Flag_of_Delaware.svg/45px-Flag_of_Delaware.svg.png"
+      },
+      {
+        name: "Florida",
+        flag: "f/f7/Flag_of_Florida.svg/45px-Flag_of_Florida.svg.png"
+      }
     ];
+
+    const showFocus = new Subject<boolean>();
+
     this.demoForm = new NgxFormGroup({
-      typeahead: new NgxBootstrapTypeaheadControl('', [Validators.required, Validators.email, Validators.minLength(3)], [], {
-        label: 'NgBootstrap Typeahead',
-        controlClass: ['form-control'],
-        ngClass: 'd-flex flex-column form-group',
-        options: typeaheadOptions,
-        defaultValue: typeaheadOptions[0],
-        inputFormatter: (item: any) => item.name,
-        openOnFocus: true,
-        errorMessages: [
-          { key: 'required', message: 'This field is required' }
-        ]
-      })
+      typeahead: new NgxBootstrapTypeaheadControl(
+        "",
+        [Validators.required, Validators.email, Validators.minLength(3)],
+        [],
+        {
+          label: "NgBootstrap Typeahead",
+          controlClass: ["form-control"],
+          ngClass: "d-flex flex-column form-group",
+          options: typeaheadOptions,
+          defaultValue: typeaheadOptions[0],
+          inputFormatter: (item: any) => item.name,
+          openOnFocus: true,
+          fullWithWindow: true,
+          focus: showFocus,
+          errorMessages: [
+            { key: "required", message: "This field is required" }
+          ]
+        }
+      )
     });
+
+    of(true)
+      .pipe(delay(5000))
+      .subscribe(val => {
+        showFocus.next(true);
+      });
   }
 
   onSubmit(): void {

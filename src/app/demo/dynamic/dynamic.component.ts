ac3099c542404be62b53f2eb9a13646c3bd194b0
hieu-pv <hieupv.est@gmail.com>
2020-12-08 12:22:55 +0700

    2 files modified

--- src/app/demo/dynamic/dynamic.component.ts
+++ src/app/demo/dynamic/dynamic.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { NgxFormGroup } from '@ngxform/platform';
-import { Validators } from '@angular/forms';
-import { catchError, debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { NgxBootstrapTypeaheadControl, WindowTemplateContext } from '@ngxform/ng-bootstrap-typeahead';
+import { Component, OnInit, TemplateRef, ViewChild } from "@angular/core";
+import { NgxFormGroup } from "@ngxform/platform";
+import { Validators } from "@angular/forms";
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  switchMap
+} from "rxjs/operators";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import {
+  NgxBootstrapTypeaheadControl,
+  WindowTemplateContext
+} from "@ngxform/ng-bootstrap-typeahead";
 
 @Component({
-  selector: 'app-dynamic',
-  templateUrl: './dynamic.component.html',
-  styleUrls: ['./dynamic.component.scss']
+  selector: "app-dynamic",
+  templateUrl: "./dynamic.component.html",
+  styleUrls: ["./dynamic.component.scss"]
 })
 export class DynamicComponent implements OnInit {
   public demoForm: NgxFormGroup;
-  @ViewChild('windowTemplate', { static: true }) windowTemplate: TemplateRef<WindowTemplateContext>;
+  @ViewChild("windowTemplate", { static: true }) windowTemplate: TemplateRef<
+    WindowTemplateContext
+  >;
 
   constructor(private http: HttpClient) {}
 
@@ -22,67 +33,97 @@ export class DynamicComponent implements OnInit {
       text$.pipe(
         debounceTime(200),
         distinctUntilChanged(),
-        switchMap((term) => {
-          if (term !== undefined && term !== '') {
-            return this.http.get(`https://restcountries.eu/rest/v2/name/${term.toLowerCase()}`).pipe(catchError((error) => error));
+        switchMap(term => {
+          if (term !== undefined && term !== "") {
+            return this.http
+              .get(
+                `https://restcountries.eu/rest/v2/name/${term.toLowerCase()}`
+              )
+              .pipe(catchError(error => error));
           } else {
-            return this.http.get('https://restcountries.eu/rest/v2/all').pipe(catchError((error) => error));
+            return this.http
+              .get("https://restcountries.eu/rest/v2/all")
+              .pipe(catchError(error => error));
           }
         }),
         map((data: any[]) => {
-          return data.map((item) => Object.assign(item, { key: item.name }));
+          return data.map(item => Object.assign(item, { key: item.name }));
         })
       );
     this.demoForm = new NgxFormGroup({
-      typeahead: new NgxBootstrapTypeaheadControl('', [Validators.required, Validators.email, Validators.minLength(3)], [], {
-        label: 'NgBootstrap Typeahead',
-        controlClass: ['form-control'],
-        ngClass: 'd-flex flex-column form-group',
-        inputFormatter: (item: any) => item.name,
-        ngbTypeahead: search,
-        windowTemplate: this.windowTemplate,
-        openOnFocus: true,
-        defaultValue: {
-          name: 'Viet Nam',
-          topLevelDomain: ['.vn'],
-          alpha2Code: 'VN',
-          alpha3Code: 'VNM',
-          callingCodes: ['84'],
-          capital: 'Hanoi',
-          altSpellings: ['VN', 'Socialist Republic of Vietnam', 'Cộng hòa Xã hội chủ nghĩa Việt Nam'],
-          region: 'Asia',
-          subregion: 'South-Eastern Asia',
-          population: 92700000,
-          latlng: [16.16666666, 107.83333333],
-          demonym: 'Vietnamese',
-          area: 331212.0,
-          gini: 35.6,
-          timezones: ['UTC+07:00'],
-          borders: ['KHM', 'CHN', 'LAO'],
-          nativeName: 'Việt Nam',
-          numericCode: '704',
-          currencies: [{ code: 'VND', name: 'Vietnamese đồng', symbol: '₫' }],
-          languages: [{ iso639_1: 'vi', iso639_2: 'vie', name: 'Vietnamese', nativeName: 'Tiếng Việt' }],
-          translations: {
-            de: 'Vietnam',
-            es: 'Vietnam',
-            fr: 'Viêt Nam',
-            ja: 'ベトナム',
-            it: 'Vietnam',
-            br: 'Vietnã',
-            pt: 'Vietname',
-            nl: 'Vietnam',
-            hr: 'Vijetnam',
-            fa: 'ویتنام'
+      typeahead: new NgxBootstrapTypeaheadControl(
+        "",
+        [Validators.required, Validators.email, Validators.minLength(3)],
+        [],
+        {
+          label: "NgBootstrap Typeahead",
+          controlClass: ["form-control"],
+          ngClass: "d-flex flex-column form-group",
+          inputFormatter: (item: any) => item.name,
+          ngbTypeahead: search,
+          windowTemplate: this.windowTemplate,
+          openOnFocus: true,
+          windowTemplate: this.windowTemplate,
+          defaultValue: {
+            name: "Viet Nam",
+            topLevelDomain: [".vn"],
+            alpha2Code: "VN",
+            alpha3Code: "VNM",
+            callingCodes: ["84"],
+            capital: "Hanoi",
+            altSpellings: [
+              "VN",
+              "Socialist Republic of Vietnam",
+              "Cộng hòa Xã hội chủ nghĩa Việt Nam"
+            ],
+            region: "Asia",
+            subregion: "South-Eastern Asia",
+            population: 92700000,
+            latlng: [16.16666666, 107.83333333],
+            demonym: "Vietnamese",
+            area: 331212.0,
+            gini: 35.6,
+            timezones: ["UTC+07:00"],
+            borders: ["KHM", "CHN", "LAO"],
+            nativeName: "Việt Nam",
+            numericCode: "704",
+            currencies: [{ code: "VND", name: "Vietnamese đồng", symbol: "₫" }],
+            languages: [
+              {
+                iso639_1: "vi",
+                iso639_2: "vie",
+                name: "Vietnamese",
+                nativeName: "Tiếng Việt"
+              }
+            ],
+            translations: {
+              de: "Vietnam",
+              es: "Vietnam",
+              fr: "Viêt Nam",
+              ja: "ベトナム",
+              it: "Vietnam",
+              br: "Vietnã",
+              pt: "Vietname",
+              nl: "Vietnam",
+              hr: "Vijetnam",
+              fa: "ویتنام"
+            },
+            flag: "https://restcountries.eu/data/vnm.svg",
+            regionalBlocs: [
+              {
+                acronym: "ASEAN",
+                name: "Association of Southeast Asian Nations",
+                otherAcronyms: [],
+                otherNames: []
+              }
+            ],
+            cioc: "VIE"
           },
-          flag: 'https://restcountries.eu/data/vnm.svg',
-          regionalBlocs: [{ acronym: 'ASEAN', name: 'Association of Southeast Asian Nations', otherAcronyms: [], otherNames: [] }],
-          cioc: 'VIE'
-        },
-        errorMessages: [
-          { key: 'required', message: 'This field is required' }
-        ]
-      })
+          errorMessages: [
+            { key: "required", message: "This field is required" }
+          ]
+        }
+      )
     });
   }
 
